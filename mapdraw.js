850e9fcb50f13152778a4f1a7683b8d3b0552184
Natalie Dodd <nataliedodd89@gmail.com>
2016-12-07 15:51:04 -0500

    map loading but initMap error is still in console

--- mapdraw.js
+++ mapdraw.js
@@ -1,7 +1,4 @@
-$( document ).ready(function() {
-
-  var poly;
-  var map;
+$(function() {
 
   var pointA_lat = 43.777167
   var pointA_lng = -79.231510
@@ -9,11 +6,16 @@ $( document ).ready(function() {
   var pointB_lat = 43.759937
   var pointB_lng = -79.224644
 
+  google.maps.event.addDomListener(window, 'load', initMap);
+
   function initMap() {
-    map = new google.maps.Map(document.getElementById('map'), {
+
+    var mapOptions = {
       center: {lat: pointA_lat, lng: pointA_lng},
       zoom: 13
-    });
+    };
+
+    var map = new google.maps.Map(document.getElementById('map'), mapOptions);
 
     var myCoordinates = [
       new google.maps.LatLng(43.777167,-79.231510),
@@ -21,35 +23,46 @@ $( document ).ready(function() {
       new google.maps.LatLng(43.761920,-79.213142)
     ];
 
-    poly = new google.maps.Polyline({
+    var poly = new google.maps.Polyline({
       path: myCoordinates,
       strokeColor: '#ba0b0b',
       strokeOpacity: 1.0,
       strokeWeight: 5
     });
+
     poly.setMap(map);
+
 
     // Add a listener for the click event
     map.addListener('click', addLatLng);
-  }
 
-  // Handles click events on a map, and adds a new point to the Polyline.
-  function addLatLng(event) {
-    var path = poly.getPath();
 
-    // Because path is an MVCArray, we can simply append a new coordinate
-    // and it will automatically appear.
-    path.push(event.latLng);
+      // Handles click events on a map, and adds a new point to the Polyline.
+       function addLatLng(event) {
+          var path = poly.getPath();
 
-    // Add a new marker at the new plotted point on the polyline.
-    var marker = new google.maps.Marker({
-      position: event.latLng,
-      title: '#' + path.getLength(),
-      map: map
-    });
-  }
+          // Because path is an MVCArray, we can simply append a new coordinate
+          // and it will automatically appear.
+          path.push(event.latLng);
 
-  initMap();
+          // Add a new marker at the new plotted point on the polyline.
+          var marker = new google.maps.Marker({
+            position: event.latLng,
+            title: '#' + path.getLength(),
+            map: map
+          });
+        }
+
+  };
+
+
+  // function loadScript() {
+  //   var script = document.createElement('script');
+  //   script.type = 'text/javascript';
+  //   script.src = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyCqsCaOw8_xtZ37g3_YWkIiwKlvMva7c7k&' +
+  //     'callback=initMap';
+  //   document.body.appendChild(script);
+  // }
 
 
 });
